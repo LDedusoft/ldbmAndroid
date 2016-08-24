4054a996a01d024370e234e7d5018c5c4edc2f4a
kingdom858 <413835711@qq.com>
2016-08-24 10:38:42 +0800

    在库车辆查询页面修改

--- app/src/main/java/com/ldedusoft/ldbm/adapters/QueryProgresslistAdapter.java
+++ app/src/main/java/com/ldedusoft/ldbm/adapters/QueryProgresslistAdapter.java
@@ -53,7 +53,7 @@ public class QueryProgresslistAdapter extends ArrayAdapter<Progress> {
             MingChengText.setText(item.getMingCheng());
             CarCodeText.setText(item.getCarCode());
         }
-        if(position%2!=0){
+        if(position%2==0){
             view.setBackgroundResource(R.color.lisetItemBackground);
         }
         return view;
